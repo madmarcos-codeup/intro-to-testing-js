0c1978ba794110dd9468e7803d5f5fe07bb85176
madmarcos-codeup <mark.robinson@codeup.com>
2023-09-13 16:09:37 -0500

    refactored to be awesome

--- tests.js
+++ tests.js
@@ -6,6 +6,15 @@ describe('sayHello', function() {
     it('should return a string when called', function() {
         expect(typeof sayHello()).toBe("string");
     });
+    it('should return "Hello, Jane" when called with "Jane"', function() {
+        expect(sayHello("Jane")).toBe("Hello, Jane!");
+    });
+    it('should return "Hello, Alex" when called with "Alex"', function() {
+        expect(sayHello("Alex")).toBe("Hello, Alex!");
+    });
+    it('should return "Hello, Pat" when called with "Pat"', function() {
+        expect(sayHello("Pat")).toBe("Hello, Pat!");
+    });
 
 });
 
